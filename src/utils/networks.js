4d8a86c0c41eb29190b92b8c40da415a617d9959
ang-st <pg@zebitex.com>
2022-08-15 22:45:11 +0200

    ADD: default testnet alias

--- src/utils/networks.js
+++ src/utils/networks.js
@@ -2,7 +2,9 @@ let networks = {
   conf:{
     mainnet:{ base:'https://api.tzstats.com/explorer', alt:"https://api.tzkt.io/v1/", rpc:'https://mainnet-tezos.giganode.io/'},
     ghostnet:{base:'https://api.ghost.tzstats.com', alt:'https://ghostnet.tzkt.io/'},
-    jakartanet:{base:' https://api.jakarta.tzstats.com', alt:"https://jakartanet.tzkt.io/", rpc:'https://testnet-tezos.giganode.io/'}
+    jakartanet:{base:' https://api.jakarta.tzstats.com', alt:"https://jakartanet.tzkt.io/", rpc:'https://testnet-tezos.giganode.io/'},
+    //default testnet
+    testnet:{base:' https://api.jakarta.tzstats.com', alt:"https://jakartanet.tzkt.io/", rpc:'https://testnet-tezos.giganode.io/'}
   }
 }
 networks.list= Object.keys(networks.conf)
